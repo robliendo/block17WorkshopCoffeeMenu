ca29edfd45b0517073369511d95d91682ef8e7e6
robliendo <126755451+robliendo@users.noreply.github.com>
2023-05-11 22:50:27 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// 3. Print an array of all the drinks on the menu.
+
+
+
+// 4. Print an array of drinks that cost 5 and under.
+
+
+
+// 5. Print an array of drinks that are priced at an even number.
+
+
+
+// 6. Print the total if you were to order one of every drink.
+
+
+
+// 7. Print an array with all the drinks that are seasonal.
+
+
+
+// 8. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
